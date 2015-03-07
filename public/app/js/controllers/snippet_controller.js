4072a87a6a08054a438a84d08bc0ef728b380a30
realalexhomer <realalexhomer@gmail.com>
2015-03-07 12:20:00 -0500

    refactor material design, take out unnecessary code

--- public/app/js/controllers/snippet_controller.js
+++ public/app/js/controllers/snippet_controller.js
@@ -1,12 +1,26 @@
 'use strict';
 
 angular.module('cmndvninja'). controller('SnippetController',
-  ['$scope', '$location', 'Snippet', 'Shared',
-  function($scope, $location, Snippet, Shared){
+  ['$scope', '$location', '$route','Snippet', 'Shared',
+  function($scope, $location, $route, Snippet, Shared){
 
-  console.log('SnippetController init', $location);
   $scope.data = {};
+  function createSnippet() {
+    console.log('hello there')
+  };
+
   $scope.greeting = 'hello there!';
+
+
+  function newProduct() {
+    $scope.mainHeader = "Create Product";
+    $scope.product = {};
+    $scope.product.is_new = true;
+    Shared.product = null;
+  }
+
+
+
   Snippet.query().$promise.then(function(snippets){
     $scope.data.snippets = snippets;
     Shared.snippets = null;
