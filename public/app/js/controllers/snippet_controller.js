a9dc9308e4bee28adad9d09d8e4d786577aa48c3
realalexhomer <realalexhomer@gmail.com>
2015-03-07 15:31:05 -0500

    gets post route working

--- public/app/js/controllers/snippet_controller.js
+++ public/app/js/controllers/snippet_controller.js
@@ -3,25 +3,28 @@
 angular.module('cmndvninja'). controller('SnippetController',
   ['$scope', '$location', '$route','Snippet', 'Shared',
   function($scope, $location, $route, Snippet, Shared){
+  
+    $scope.formData = {
+      user: '54f9fe5f2913f1f928a09fea'
+    };
 
-  $scope.createSnippet = function () {
-    console.log('hello')
-  };
+
+    $scope.data = {};
+    $scope.createSnippet = function () {
+      console.log($scope.formData)
+      Snippet.post($scope.formData)
+    };
+
 
   $scope.snippets = ["snippet", "snippet1"]
   $scope.greeting = 'hello there!';
 
 
-  // function newSnippet() {
-  //   $scope.mainHeader = "Create Product";
-  //   $scope.snippet= {};
-  // }
 
-
-
-  // Snippet.query().$promise.then(function(snippets){
-  //   $scope.data.snippets = snippets;
-  //   Shared.snippets = null;
-  // });
+  Snippet.query().$promise.then(function(snippets){
+    $scope.data.snippets = snippets;
+    Shared.snippets = null;
+    console.log(snippets)
+  });
 
 }]);
