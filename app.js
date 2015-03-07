957e58d64fabb1af4fb46b176280276f46d4b8d5
realalexhomer <realalexhomer@gmail.com>
2015-03-07 12:40:07 -0500

    clean up ace editor to include what we need

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/ace_editor',  express.static(__dirname + '/ace_editor'));
 
 
 
