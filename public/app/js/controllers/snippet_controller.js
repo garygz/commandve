c028e046892b8b107c6b59de8032dc7993218d93
realalexhomer <realalexhomer@gmail.com>
2015-03-07 14:50:02 -0500

    get stylehseets to load

--- public/app/js/controllers/snippet_controller.js
+++ public/app/js/controllers/snippet_controller.js
@@ -4,26 +4,24 @@ angular.module('cmndvninja'). controller('SnippetController',
   ['$scope', '$location', '$route','Snippet', 'Shared',
   function($scope, $location, $route, Snippet, Shared){
 
-  $scope.data = {};
-  function createSnippet() {
-    console.log('hello there')
+  $scope.createSnippet = function () {
+    console.log('hello')
   };
 
+  $scope.snippets = ["snippet", "snippet1"]
   $scope.greeting = 'hello there!';
 
 
-  function newProduct() {
-    $scope.mainHeader = "Create Product";
-    $scope.product = {};
-    $scope.product.is_new = true;
-    Shared.product = null;
-  }
+  // function newSnippet() {
+  //   $scope.mainHeader = "Create Product";
+  //   $scope.snippet= {};
+  // }
 
 
 
-  Snippet.query().$promise.then(function(snippets){
-    $scope.data.snippets = snippets;
-    Shared.snippets = null;
-  });
+  // Snippet.query().$promise.then(function(snippets){
+  //   $scope.data.snippets = snippets;
+  //   Shared.snippets = null;
+  // });
 
 }]);
