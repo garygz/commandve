62ab01a6851dd663cfa3251c0ecf4cabc9501753
Kenn Breece <kjb085@gmail.com>
2015-03-08 19:23:20 -0400

    Start work on a function to hide the navbar when on the login route - currently not working

--- public/app/js/controllers/header_controller.js
+++ public/app/js/controllers/header_controller.js
@@ -1,10 +1,13 @@
-angular.module('cmndvninja').controller('HeaderController', ['$scope', 'Shared', function($scope, Shared){
+angular.module('cmndvninja').controller('HeaderController', ['$scope', '$location', 'Shared', function($scope, $location, Shared){
   console.log('Header init');
 
   // $scope.$watch( function() { return Shared; }, function(data) {
   //   console.log('Shared init');
   // }, true);
 
-
+  // This should hide the nav given the location designated in the 
+  $scope.hideNav = function(viewLocation){
+  	return viewLocation === $location.path();
+  }
 
 }]);
