e6c09c503dd59a362d14b32081343e65ca05001d
Kenn Breece <kjb085@gmail.com>
2015-03-08 19:22:18 -0400

    Create client side routes to render html for user profile, login, and signup

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -2,8 +2,12 @@
 
 /* App Module */
 
+<<<<<<< HEAD
 angular.module('cmndvninja', ['ngResource', 'ngRoute', 'ngMessages', 'ui.ace',
                               'ui.bootstrap']);
+=======
+angular.module('cmndvninja', ['ngResource', 'ngRoute', 'ngMessages', 'ngCookies', 'ui.ace']);
+>>>>>>> Create client side routes to render html for user profile, login, and signup
 
 angular.module('cmndvninja'). config(['$routeProvider',function($routeProvider) {
 
@@ -17,15 +21,33 @@ angular.module('cmndvninja'). config(['$routeProvider',function($routeProvider) 
       controller: 'SnippetController'
     });
 
+    // Do we really need a list of all users page?
     $routeProvider.when('/users', {
       templateUrl: 'app/partials/user-list.html',
       controller: 'UserController'
     });
 
+<<<<<<< HEAD
     $routeProvider.when('/search', {
       templateUrl: 'app/partials/search/search-results.html',
       controller: 'SearchController'
     });
+=======
+    $routeProvider.when('/users/profile', {
+      templateUrl: 'app/partials/users/profile.html',
+      controller: 'ProfileController'
+    })
+
+    $routeProvider.when('/login', {
+      templateUrl: 'app/partials/auth/login.html',
+      controller: 'AuthController' // This might need to be user controller instead or might just make things easier
+    })
+
+    $routeProvider.when('/signup', {
+      templateUrl: 'app/partials/auth/signup.html',
+      controller: 'AuthController'
+    })
+>>>>>>> Create client side routes to render html for user profile, login, and signup
 
     $routeProvider.otherwise({redirectTo: '/groups'});
 
