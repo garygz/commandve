51f0a260f22257f64bfb1b44e888956f1e1ad73f
Kenn Breece <kjb085@gmail.com>
2015-03-08 21:57:02 -0400

    Update login and sign up methods to receieve appropriate response from the server

--- public/app/js/controllers/auth_controller.js
+++ public/app/js/controllers/auth_controller.js
@@ -11,16 +11,23 @@ angular.module('cmndvninja').controller('AuthController',
   $scope.login = function(user_info){
     console.log(user_info); // This is approprately passing username and password
     auth.login(user_info).$promise.then(function(data){
-      $cookies.put('token', data._id);
-    });
-    $location.path('/#/users/profile').replace();
+      console.log(data);
+    });  
   };
 
   $scope.signup = function(user_info){
-    User.post(user_info).$promise.then(function(data){
-      $cookies.put('token', data._id);
-    });
-    $location.path('/#/users/profile').replace();
+    User.post(user_info).$promise.then(
+      function(data){
+        if(data instanceof Array){
+          document.getElementsByClassName('initiallyHidden').className.replace(/\binitiallyHidden\b/, '')
+        }
+        else{
+          console.log(data);
+          $cookies.put('token', data._id);
+          $location.path('/#/users/profile').replace();
+        }
+      }
+    );
   };
 
 }]);
