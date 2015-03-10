f9478d0e48e460947f705266a25279bbca58eda1
garygz <garyginzburg@hotmail.com>
2015-03-10 19:55:29 -0400

    Add logging to object id casting

--- helpers/mongoose_objectid.js
+++ helpers/mongoose_objectid.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
 exports.castToObjectId = function(stringId){
+
   var id = mongoose.Types.ObjectId(stringId);
+  console.log("cast objectid", id);
   return id;
 }
 
