465e7f42ebce86d6d1dbefa5912f940c1fea01b8
Alex Homer <realalexhomer@gmail.com>
2015-03-10 15:10:23 -0400

    add image to group html

--- public/app/js/controllers/snippet_controller.js
+++ public/app/js/controllers/snippet_controller.js
@@ -4,6 +4,8 @@ angular.module('cmndvninja'). controller('SnippetController',
   ['$scope', '$location', '$route','Snippet', 'Shared',
   function($scope, $location, $route, Snippet, Shared){
 
+    $.material.init()
+
     $scope.samples = ["group 1", "group 2", "group 3", "group 4", "group 5", "group 6", "group 7"]
     var getGroupId = function(){
       var url = $location.absUrl();
