c33bf315bf3649f8af8e213e13ebc6f280975b77
garygz <garyginzburg@hotmail.com>
2015-03-10 09:59:02 -0400

    Refactor user resolution code and get github email

---
+++ helpers/https.js
@@ -0,0 +1,78 @@
+
+var https = require("https");
+
+
+exports.httpGet = function(options, callbackSuccess, callbackError){
+    console.log('http get',options);
+    try{
+    var getReq = https.get(options, function (http_res) {
+      console.log('Response is '+http_res.statusCode);
+      // initialize the container for our data
+      var data = "";
+
+      // this event fires many times, each time collecting another piece of the response
+      http_res.on("data", function (chunk) {
+          // append this chunk to our growing `data` var
+          data += chunk;
+      });
+
+      // this event fires *one* time, after all the `data` events/chunks have been gathered
+      http_res.on("end", function () {
+          // you can use res.send instead of console.log to output via express
+          console.log("github data in ", data);
+          if(data.indexOf('error')>-1 || http_res.statusCode>299){
+            //expired token
+             callbackError(data);
+          }else{
+            callbackSuccess(data);
+          }
+
+
+      });
+    });
+
+
+  }catch(e){
+    console.log(e.stack);
+    callbackError(e);
+  }
+}
+
+
+
+exports.httpPost = function(options, data,callbackSuccess, callbackError){
+  try{
+    var postReq = https.request(options, function (http_res) {
+      console.log('Response is '+http_res.statusCode);
+      // initialize the container for our data
+      var data = "";
+
+      // this event fires many times, each time collecting another piece of the response
+      http_res.on("data", function (chunk) {
+          // append this chunk to our growing `data` var
+          data += chunk;
+      });
+
+      // this event fires *one* time, after all the `data` events/chunks have been gathered
+      http_res.on("end", function () {
+          // you can use res.send instead of console.log to output via express
+          console.log("github data in ", data);
+          if(data.indexOf('error')>-1 || http_res.statusCode>299){
+            //expired token
+             callbackError(data);
+          }else{
+            callbackSuccess(data);
+          }
+
+
+      });
+    });
+    // post the data
+    postReq.write(data);
+    postReq.end();
+
+  }catch(e){
+    console.log(e.stack);
+    callbackError(e);
+  }
+}
---
+++ helpers/users.js
@@ -0,0 +1,53 @@
+var UserModel = null;
+
+exports.setUserModel = function(User){
+  UserModel = User;
+}
+
+exports.findUser = function(githubId, callbackSuccess, callbackError){
+  console.log("find user", githubId);
+  UserModel.findOne({githubId:githubId}, function(error, user){
+
+     if(error)  {
+          console.log("find user error", error);
+          callbackError(error);
+      }else{
+
+         console.log("found user", user);
+         callbackSuccess(user);
+      }
+  });
+}
+
+exports.createUser = function(profile, callbackSuccess, callbackError){
+  var createArgs = {username: profile.login,
+                email:profile.email || '<TBD>',//email might be resolved later as it's another request for some OAuth providers
+                githubId: profile.id,
+                token: profile.token,
+                gists_url: profile.gists_url
+              };
+
+  console.log("create user", createArgs);
+   UserModel.create(createArgs, function(error, user){
+        if(error)  {
+         callbackError(error);
+        }else{
+          callbackSuccess(user);
+        }
+
+  });
+}
+
+exports.findOrCreateUser = function(profile, callbackSuccess, callbackError){
+  console.log("findOrCreate", profile["id"]);
+  exports.findUser(profile.id,function(user){
+    if(user){
+      callbackSuccess(user);
+    }else{
+      //get email
+      createUser(profile, callbackSuccess, callbackError);
+    }
+  });
+}
+
+
